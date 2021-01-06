4da4d7022232b38171f7502cc2a4f51249ce8389
Parterdev <litemeilateran@outlook.com>
2021-01-05 23:27:40 -0500

    Add route to link storage

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\Backend\PostController;
 
+//Ruta para crear el link simbolico de storage
+Route::get('storage-link', function () {
+    Artisan::call('storage:link');
+});
+
 //Definimos la ruta para lista de Posts
 Route::get('/', [PageController::class,'posts']);
 
